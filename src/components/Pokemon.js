58c334f4b8e2992cfc8de8d3990c112a15c4a1d0
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2021-12-23 13:39:06 +0100

    update 23/12

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,75 +1,88 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import { PokeCard } from "./Cards";
 import { PokeService } from "../services/PokeService";
-import {
-  CircularProgress,
-  Typography,
-  appBar,
-  Card,
-  CardMedia,
-  CardContent,
-  CssBaseline,
-  Grid,
-  Container,
-  CardActionArea,
-  Box,
-  button,
-} from "@mui/material";
+import { Box, CircularProgress, Grid } from "@mui/material";
 
-const Pokemon = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    PokeService.fetchFullPokemons().then((res) => {
-      setPokemons(res);
-      setLoaded(true);
-    });
-  }, []);
-
-  const PokeCards =
-    pokemons &&
-    pokemons.map((pokemon) => (
-      <Grid
-        item
-        xl={2}
-        xs={12}
-        sm={6}
-        md={4}
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link className="linkpokemon" to="/details" state={{ pokemondetails: pokemon }}>
-          <PokeCard pokemon={pokemon} />
-        </Link>
-      </Grid>
-    ));
-
-  if (loaded === false) {
-    return (
-      <Box
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <CircularProgress />
-      </Box>
-    );
-  } else {
-    return (
-      <Grid container spacing={2}>
-        {PokeCards}
-      </Grid>
-    );
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [],
+      loaded: false,
+      limit: 30,
+    };
   }
-};
+
+  componentDidMount = () => {
+    window.onscroll = (e) => {
+      if (
+        window.innerHeight + window.pageYOffset >=
+        document.body.offsetHeight - 300
+      ) {
+        this.setState({ limit: this.state.limit + 30 });
+      }
+    };
+
+    PokeService.fetchFullPokemons().then((res) => {
+      this.setState({ pokemons: res, loaded: true });
+    });
+  };
+
+  PokeCards = () => {
+    const { pokemons, limit } = this.state;
+    return (
+      pokemons &&
+      pokemons.slice(0, limit).map((pokemon) => (
+        <Grid
+          item
+          xl={2}
+          xs={12}
+          sm={6}
+          md={4}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Link
+            className="linkpokemon"
+            to="/details"
+            state={{ pokemondetails: pokemon }}
+          >
+            <PokeCard pokemon={pokemon} />
+          </Link>
+        </Grid>
+      ))
+    );
+  };
+
+  render() {
+    console.log(this);
+    const { loaded } = this.state;
+
+    if (loaded === false) {
+      return (
+        <Box
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      );
+    } else {
+      return (
+        <Grid container spacing={2}>
+          <this.PokeCards />
+        </Grid>
+      );
+    }
+  }
+}
 
 export default Pokemon;
